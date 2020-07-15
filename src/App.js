d2934febc0668f2532cc6b7a3c29e0ca10e1146a
Cory Zue <czue@dimagi.com>
2020-07-15 15:32:10 +0200

    pass credentials to api explorer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, {useState}  from 'react';
+
 import './App.css';
 import ApiExplorer from "./commcare/ApiExplorer";
 import ReportDashboard from "./commcare/ReportDashboard";
 
 function App() {
-  const [tab, setTab] = useState('explorer');
+  const [username, setUsername] = useState(process.env.REACT_APP_COMMCARE_DEFAULT_USERNAME);
+  const [apiKey, setApiKey] = useState(process.env.REACT_APP_COMMCARE_DEFAULT_API_KEY);
+  const [tab, setTab] = useState('dashboard');
   let inner;
   if (tab === 'explorer') {
-    inner = <ApiExplorer />
+    inner = <ApiExplorer username={username} apiKey={apiKey}
+                         setUsername={(username) => setUsername(username)}
+                         setApiKey={(apiKey) => setApiKey(apiKey)}/>
   } else {
-    inner = <ReportDashboard />
+    inner = <ReportDashboard username={username} apiKey={apiKey} />
   }
   return (
     <div className="App">
--- src/commcare/ApiExplorer.js
+++ src/commcare/ApiExplorer.js
@@ -4,16 +4,14 @@ import {fetchCommCareApi} from "./Client";
 
 
 
-function ApiExplorer() {
-  const [username, setUsername] = useState(process.env.REACT_APP_COMMCARE_DEFAULT_USERNAME);
-  const [apiKey, setApiKey] = useState(process.env.REACT_APP_COMMCARE_DEFAULT_API_KEY);
+function ApiExplorer(props) {
   const [api, setApi] = useState(process.env.REACT_APP_COMMCARE_DEFAULT_API);
   const [apiData, setApiData] = useState('')
   const [isLoading, setIsLoading] = useState(false);
   const hitApi = function () {
     setIsLoading(true);
     fetchCommCareApi(
-      api, username, apiKey, {
+      api, props.username, props.apiKey, {
         onSuccess: (response) => {
           setIsLoading(false);
           setApiData(JSON.stringify(response, null, 2));
@@ -31,11 +29,11 @@ function ApiExplorer() {
   return (
     <div className="ApiExplorer">
       <h1>CommCare API Explorer</h1>
-      <ApiKey username={username} apiKey={apiKey}
-              onUsernameChanged={(username) => setUsername(username)}
-              onApiKeyChanged={(apiKey) => setApiKey(apiKey)}
+      <ApiKey username={props.username} apiKey={props.apiKey}
+              onUsernameChanged={(username) => props.setUsername(username)}
+              onApiKeyChanged={(apiKey) => props.setApiKey(apiKey)}
       />
-      <p>Your username is <strong>{username}</strong> and your api key is <strong>{apiKey}</strong></p>
+      <p>Your username is <strong>{props.username}</strong> and your api key is <strong>{props.apiKey}</strong></p>
       <h2>API Url</h2>
       <input type="text" style={{width: "60em"}} value={api} onChange={(event) => setApi(event.target.value)}/>
       <br />
