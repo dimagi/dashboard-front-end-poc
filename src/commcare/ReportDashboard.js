979013efe42d2fb5f634567ea088d8425a4eb5a6
Cory Zue <czue@dimagi.com>
2020-07-16 11:13:33 +0200

    add report list

--- src/commcare/ReportDashboard.js
+++ src/commcare/ReportDashboard.js
@@ -1,11 +1,15 @@
 import React, {useState, useEffect}  from 'react';
 import {fetchCommCareApi} from "./Client";
+import {listReports} from "./Reports";
 
 function ReportDashboard(props) {
   const aggregateReport = process.env.REACT_APP_COMMCARE_AGGREGATE_REPORT;
   const listReport = process.env.REACT_APP_COMMCARE_LIST_REPORT;
+  const [domain, setDomain] = useState(process.env.REACT_APP_COMMCARE_DOMAIN);
   const [aggregateData, setAggregateData] = useState({});
   const [listData, setListData] = useState({});
+  const [allReports, setAllReports] = useState([]);
+
 
   const ALL = 'all';
   const choiceFilterOptions = [ALL, "hiking", "running", "surfing"];
@@ -13,10 +17,19 @@ function ReportDashboard(props) {
   const [selectedDate, setSelectedDate] = useState(null);
 
   useEffect(() => {
+    listReports(
+      props.username, props.apiKey, domain, {
+        onSuccess: setAllReports,
+      });
+  }, [domain]);
+
+
+  useEffect(() => {
     const params = {};
     if (selectedChoice !== ALL) {
       params.activity_name_077ac727_string_1 = selectedChoice;
     }
+
     fetchCommCareApi(
       aggregateReport, props.username, props.apiKey, {
         onSuccess: setAggregateData,
@@ -32,6 +45,10 @@ function ReportDashboard(props) {
 
   return (
     <div className="ReportDashboard">
+      <p>Domain</p>
+      <input type="text" value={domain} onChange={(event) => setDomain(event.target.value)}/>
+      <h2>All Reports in {domain}</h2>
+      <ReportList reports={allReports}></ReportList>
       <h2>Filters</h2>
       <p>Type</p>
       <select onChange={(event) => setSelectedChoice(event.target.value)}>
@@ -39,9 +56,9 @@ function ReportDashboard(props) {
           return <option key={index} value={choice} >{choice}</option>
         })}
       </select>
-      <h2>Aggregate data</h2>
+      <h2>Summary: Cases Reported Today</h2>
       <ListView {...aggregateData} />
-      <h2>List data</h2>
+      <h2>List: Cases Reported Today</h2>
       <ListView {...listData} />
 
       <h2>Debug data</h2>
@@ -76,5 +93,29 @@ function ListView(props) {
   )
 }
 
-
+function ReportList(props) {
+  if (props.reports.length) {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Report</th>
+          </tr>
+        </thead>
+        <tbody>
+        {props.reports.map((report, index) => {
+          return (
+            <tr key={index}>
+              <td>{report.title}</td>
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
+    )
+  } else {
+    return <p>No reports found...</p>
+  }
+}
 export default ReportDashboard;
+
